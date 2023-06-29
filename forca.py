68e7d2f02547adf73868f1bf4a33e40840153bbd
pedroarouck <pedrohenrique.sales@hotmail.com>
2023-06-29 02:55:34 +0200

    refactor do jogo da forca

--- forca.py
+++ forca.py
@@ -1,8 +1,37 @@
-def jogar():
-    import random
+import random
 
+
+def jogar():
+    mensagem_de_abertura()
+    palavra_secreta = pega_palavras_secretas()
+    letras_acertadas = configura_letras_acertadas(palavra_secreta)
+
+    acertou = False
+    enforcou = False
+    erros = 0
+
+    while (not acertou and not enforcou):
+        chute = pedir_chute()
+        if (chute in palavra_secreta):
+            chute_certo(chute, palavra_secreta, letras_acertadas)
+        else:
+            erros += 1
+        print(letras_acertadas)
+
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
+
+    if (acertou):
+        print('Voce ganhou')
+    else:
+        print('Voce foi enforcado')
+
+
+def mensagem_de_abertura():
     print("Bem vindo ao jogo da forca")
 
+
+def pega_palavras_secretas():
     arquivo = open('palavras.txt', 'r')
     palavras = []
 
@@ -11,41 +40,23 @@ def jogar():
         palavras.append(linha)
 
     arquivo.close()
-
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ['_' for letra in palavra_secreta]
-
-    acertou = False
-    enforcou = False
-    erros = 0
-
-    while(not acertou and not enforcou):
-        chute = input('Qual letra?\n').upper()
-        if(chute in palavra_secreta):
-            posicao = 0
-
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[posicao] = letra
-                posicao += 1
-
-        else:
-            erros += 1
-        print(letras_acertadas)
-
-        enforcou = erros == 6
-        acertou = '_' not in letras_acertadas
-
-    if(acertou):
-        print('Voce ganhou')
-    else:
-        print('Voce foi enforcado')
+    return palavra_secreta
 
 
+def configura_letras_acertadas(palavra):
+    return ['_' for letra in palavra]
 
 
+def pedir_chute():
+    chute = input('Qual letra?\n').upper()
+    return chute
 
 
-
+def chute_certo(chute, palavra_secreta, letras_acertadas):
+    posicao = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[posicao] = letra
+        posicao += 1
