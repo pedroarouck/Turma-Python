3704963a61c3b97eeaa421b0a3c40b16a8463e9d
pedroarouck <pedrohenrique.sales@hotmail.com>
2023-07-04 00:32:08 +0200

    termino da implementação da classe historico

--- Banco/conta.py
+++ Banco/conta.py
@@ -1,4 +1,5 @@
 from cliente import Cliente
+from historico import Historico
 class Conta:
     def __init__(self, numero, cliente, saldo, limite):
         print('Inicializando a conta')
@@ -6,19 +7,23 @@ class Conta:
         self.titular = cliente.nome
         self.saldo = saldo
         self.limite = limite
+        self.historico = Historico()
 
     def depositar(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append("depósito de {} reais".format(valor))
 
     def sacar(self, valor):
         if(self.saldo < valor):
             return False
         else:
             self.saldo -= valor
+            self.historico.transacoes.append("saque de {} reais".format(valor))
             return True
 
     def extrato(self):
         print("numero: {} \ntitular: {} \nsaldo: {}".format(self.numero, self.titular, self.saldo))
+        self.historico.transacoes.append("tirou o extrato - saldo {} reais".format(self.saldo))
 
     def transferir(self, destino, valor):
         retirada = self.sacar(valor)
@@ -26,4 +31,5 @@ class Conta:
             return False
         else:
             destino.depositar(valor)
+            self.historico.transacoes.append("envio de {} reais sacados para a conta {}".format(valor, destino.numero))
             return True
